d579fc988efa5e313c8312ec43d63e658d1378dd
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2021-09-04 23:00:08 +0300

    add  logo and create som css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,41 +1,13 @@
 import React from "react";
 import "./Home.css";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import logo from "./image Navbar/logo_ecomblack_lg.png";
 const Home = () => {
   return (
     <div className="content-hoome">
-      <div className="Navbar">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+      <div className="Navbar-general">
+        <div className="logo-navbar">
+          <img src={logo} className="img-logo" alt="image" />
+        </div>
       </div>
     </div>
   );
