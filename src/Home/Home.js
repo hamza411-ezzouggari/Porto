d55ee9ff410253517f6fcf29199f330a23ec6f96
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2021-09-05 03:18:49 +0300

    create navbar

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,9 +5,20 @@ const Home = () => {
   return (
     <div className="content-hoome">
       <div className="Navbar-general">
-        <div className="logo-navbar">
-          <img src={logo} className="img-logo" alt="image" />
-        </div>
+        <nav className="nav">
+          <ul className="menu-bar">
+            <div className="logo-navbar">
+              <img src={logo} className="img-logo" alt="images" />
+            </div>
+            <li>HOME</li>
+            <li>CATEGORIES</li>
+            <li>PRODUCTS</li>
+            <li>PAGES</li>
+            <li>FEATURES</li>
+            <li>BLOG</li>
+            <li>BUY PORTO!</li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
