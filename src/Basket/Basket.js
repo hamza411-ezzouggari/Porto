0492b8993de1b43d775bc8046143315665d4891b
hamza411-ezzouggari <hamzahamaza256@gmail.com>
2021-10-06 21:04:19 +0300

    create fother for basket

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -5,7 +5,12 @@ import banner from "../Home/image Navbar/menu-banner.jpg";
 import loope from "../Home/image Navbar/loupe.png";
 import user from "../Home/image Navbar/user.png";
 import shopping from "../Home/image Navbar/shopping-bag.png";
+import payment from "../Home/image Navbar/shop2_payment_logo.png";
+import { ImFacebook } from "react-icons/im";
+import { ImTwitter } from "react-icons/im";
+import { ImInstagram } from "react-icons/im";
 import "./Basket.css";
+import "../Home/Home.css";
 function basket() {
   return (
     <div className="Basket">
@@ -230,6 +235,167 @@ function basket() {
             </div>
           </ul>
         </nav>
+      </div>
+      <div className="section">
+        <section></section>
+      </div>
+      <div className="fother-Section-principal-basket">
+        <div className="porto-separator-taller-tow">
+          <hr className="align_center-hr-basket" />
+        </div>
+        <div className="col-lg-12">
+          <div className="widget_text">
+            <div className="textwidget">
+              <h5>Subscribe newsletter</h5>
+              <p>Get all the latest information on Events, Sales and Offers.</p>
+            </div>
+            <div className="block-newsletter">
+              <div className="add55">
+                <input
+                  className="input-add55"
+                  name="email"
+                  type="email"
+                  id="footer_newsleter"
+                  data-validate="{required:true,'validate-email':true}"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="action34">
+                <button
+                  className="action34 subscribe primary"
+                  title="Subscribe"
+                  type="submit"
+                >
+                  <span>Subscribe</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="widget-follow-us">
+            <div className="Share-liks">
+              <a href="#" target="_blank" title="facebook">
+                <ImFacebook className="share-facebook" />
+              </a>
+              <a href="#" target="_blank" title="twitter">
+                <ImTwitter className="share-twitter" />
+              </a>
+              <a href="#" target="_blank" title="Instegram">
+                <ImInstagram className="share-Instegram" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="porto-separator-taller-tree">
+          <hr className="align_center-hr" />
+        </div>
+        <div className="footer-middle">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="widgete__text">
+                  <h3>CONTACT INFO</h3>
+                </div>
+                <div className="contact-info-block">
+                  <ul className="contact-name-block">
+                    <li className="far-fa-dot-circle">
+                      <strong>ADDRESS :</strong>
+                      <span> 123 Street name, City, England</span>
+                    </li>
+                    <li className="fab-fa-whatsapp">
+                      <strong>PHONE :</strong>
+                      <span> (123) 456-789 </span>
+                    </li>
+                    <li className="far-fa-envelope">
+                      <strong>Email :</strong>
+                      <span>
+                        <a href="mailto:mail@example.com">mail@example.com</a>
+                      </span>
+                    </li>
+                    <li className="far-fa-clock">
+                      <strong>WORKING DAY/HOURS :</strong>
+                      <span>Mon - Sun / 9:00 AM 8:00 PM</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-5">
+                <div className="widgete__text">
+                  <h3>MY ACCOUNT</h3>
+                </div>
+                <div className="textwidget">
+                  <div className="row-d-inline-flex">
+                    <div className="colx-xl-6">
+                      <ul>
+                        <li>
+                          <a href="#">About Us</a>
+                        </li>
+                        <li>
+                          <a href="#">Contact Us</a>
+                        </li>
+                        <li>
+                          <a href="#">My Account</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="colx-xl-6">
+                      <ul className="order-history">
+                        <li>
+                          <a href="#">Orders history</a>
+                        </li>
+                        <li>
+                          <a href="#">advanced Search</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="widgete__text">
+                  <h3>Main Features</h3>
+                  <div className="textwidget_6">
+                    <div className="row-d-inline-flex_7">
+                      <div className="colx-xl-7">
+                        <ul>
+                          <li>
+                            <a href="#">Super Fast WordPress Theme</a>
+                          </li>
+                          <li>
+                            <a href="#">1st Fully working Ajaz Them</a>
+                          </li>
+                          <li>
+                            <a href="#">36 unique Shop Layouts </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="colx-xl-7">
+                      <ul className="order-history_7">
+                        <li>
+                          <a href="#">Powerful Admin Panel</a>
+                        </li>
+                        <li>
+                          <a href="#">Mobile & Ratina Optimized</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="porto-separator-taller-foor">
+          <hr className="align_center-hr" />
+        </div>
+        <div className="footer-middle">
+          <div className="container-footer">
+            <address>©Copyright 2021 by PORTO. All Rights Reserved.</address>
+            <div className="custom-block-right">
+              <img className="bottom-payment-icon" src={payment} alt="imges" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
